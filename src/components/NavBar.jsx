9c3ea781969236dc3d47faffbc397f060e2f1a06
Kwok <kwokrenx@gmail.com>
2020-10-15 23:02:41 -0400

    added icon on navbar prototype

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = (props) => {
+    return (
+        <div className="navbar">
+            <div className="navbar-brand">
+                <img src="https://res.cloudinary.com/dpggcudix/image/upload/v1600736568/KR_-_Logo-06_qka7s3.png"></img>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
