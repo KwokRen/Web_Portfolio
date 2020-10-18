5fb8236fd3a5208befbf8b1fe3050e0634c8ec1b
Kwok <kwokrenx@gmail.com>
2020-10-18 10:29:02 -0400

    add nav styles and flex end the nav bar

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo = (props) => {
+    return (
+        <div className="logo-container">
+            <div className="logo"></div>
+            <h4 className="logo-name">Kwok Ren</h4>
+        </div>
+    )
+}
+
+export default Logo
