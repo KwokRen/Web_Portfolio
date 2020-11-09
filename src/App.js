7a57ca1535ff62a6dacc42c916909f2b021ba7e1
Kwok <kwokrenx@gmail.com>
2020-11-09 10:07:15 -0500

    working on banner overlay and hamburger

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import React from 'react';
 import './App.css';
 // import NavbarTwo from './components/Navbar2'
 import Banner from './components/Banner'
+import Hamburger from './components/Hamburger';
 
 function App() {
   return (
     <div className="App">
+      <Hamburger />
       {/* <NavbarTwo /> */}
       <Banner />
     </div>
